98d275c276afae4c354040c296c1aa4dd121ca0f
Mohammed Sadik <msadikjowel@gmail.com>
2021-11-11 18:01:50 +0600

    private route and login, register part done

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT | 5000;
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
-
+// mongo connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxr0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// main function
 async function run() {
     try {
         await client.connect();
@@ -19,6 +23,7 @@ async function run() {
         const database = client.db('watchStore');
         const productsCollection = database.collection('products');
         const reviewsCollection = database.collection('reviews');
+        const purchasedCollection = database.collection('purchasedProducts')
 
 
         // GET API (get exact 6 products for homepage)
@@ -33,11 +38,29 @@ async function run() {
             res.json(result);
         });
 
+        // GET API (get single product to purchase)
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+
+        })
+
         // GET API (get all reviews)
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find({}).toArray();
             res.json(result);
         });
+
+
+        // POST API (set purhased products to database)
+        app.post('/purchased', async (req, res) => {
+            const purchased = req.body;
+
+            const result = await purchasedCollection.insertOne(purchased);
+            res.json(result);
+        })
 
 
 
