fc30ac30c1095b804e2952edead5cbfb3c4a73ea
Mohammed Sadik <msadikjowel@gmail.com>
2021-11-13 11:42:53 +0600

    solved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
